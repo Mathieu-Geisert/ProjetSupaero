2a2f1609c5712cefdfb8c9a7c2469361bbd51688
Valenza Florian <fvalenza@laas.fr>
2016-02-08 19:04:56 +0100

    Reworked cmake. Should be easier to use.

---
+++ include/my-file.hpp
@@ -0,0 +1,16 @@
+#ifndef __MY_FILE_HPP__
+#define __MY_FILE_HPP__
+
+#include <iostream>
+
+class TOTO
+{
+public:
+  TOTO();
+  // ~TOTO();
+
+  void disp();
+  
+};
+
+#endif // #define __MY_FILE_HPP__
---
+++ tests/test.cpp
@@ -0,0 +1,16 @@
+#define AVOID_SINGULARITIES 1
+
+#include "gepetto/viewer/corba/client.hh"
+#include "my-file.hpp"
+
+int main( ){
+
+    TOTO tot;
+    tot.disp();
+    std::cout << "Hello" << std::endl;
+    // graphics::corbaServer::ClientCpp client = graphics::corbaServer::ClientCpp();
+    // client.createWindow("window");
+
+    return 0;
+}
+/* <<< end tutorial code <<< */
